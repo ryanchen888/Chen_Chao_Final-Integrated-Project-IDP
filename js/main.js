e09662cc07dfe79211f305b3a45fccc3ee59cac0
CCryan888 <113542854+ryanchen888@users.noreply.github.com>
2023-04-09 11:49:53 -0400

    main files
    add main files

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function(){
+	"use strict";	
+	console.log("fired");
+
+	let button = document.querySelector("#button");
+	let burgerCon = document.querySelector("#burger-con");
+
+	function hamburgerMenu() {
+		burgerCon.classList.toggle("slide-toggle");
+		button.classList.toggle("expanded");
+	};
+
+	// let hamburgerMenu = () => {
+	// 	burgerCon.classList.toggle("slide-toggle");
+	// 	button.classList.toggle("expanded");
+	// };
+
+	button.addEventListener("click", hamburgerMenu, false);		
+})();
